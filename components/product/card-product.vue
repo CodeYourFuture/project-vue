ee5818ddea5cea338886b6e65e6f66a15e537af4
Lunbin Yang <yanglunbin@cct.cn>
2017-02-10 14:48:20 +0800

    父组件和子组件之间通信

--- components/product/card-product.vue
+++ components/product/card-product.vue
@@ -1,11 +1,12 @@
 <template>
 	<div class="card-product">
-	{{ product.tag }}
+		<p>{{ product.tag }}</p>
+		<p>{{ product.name }}</p>
 	</div>
 </template>
 
 <style scoped>
-	.card-product{width: 200px; height: 150px; margin: 20px; border: 1px solid #eee;}
+	.card-product{display: inline-block; width: 250px; height: 150px; margin: 20px; border: 1px solid #eee; font-size: 14px;}
 </style>
 
 <script>
--- components/product/products.vue
+++ components/product/products.vue
@@ -1,12 +1,12 @@
 <template>
 	<section class="products">
 		<card-product v-for="product in products"
-					  ></card-product>
+					  :product="product"></card-product>
 	</section>
 </template>
 
 <style scoped>
-	.products{width: 1200px; margin: 30px auto;  border: 1px solid #eee;}
+	.products{width: 1200px; margin: 30px auto; font-size: 0;}
 </style>
 
 <script>
@@ -16,12 +16,44 @@
 			return {
 				products: [
 					{
-						tag : '',
+						tag : '出境游',
 						name: '欧洲14国1日游',
 					},
 					{
-						tag : '',
+						tag : '自由行',
+						name: '南极40年游',
+					},
+					{
+						tag : '出境游',
 						name: '欧洲14国1日游',
+					},
+					{
+						tag : '自由行',
+						name: '南极40年游',
+					},
+					{
+						tag : '出境游',
+						name: '欧洲14国1日游',
+					},
+					{
+						tag : '自由行',
+						name: '南极40年游',
+					},
+					{
+						tag : '出境游',
+						name: '欧洲14国1日游',
+					},
+					{
+						tag : '自由行',
+						name: '南极40年游',
+					},
+					{
+						tag : '出境游',
+						name: '欧洲14国1日游',
+					},
+					{
+						tag : '自由行',
+						name: '南极40年游',
 					},
 				]
 			}
