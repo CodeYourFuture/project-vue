e21a290a0c59daa58403d6b34d3bbc72c382a2da
Lunbin Yang <yanglunbin1992@gmail.com>
2017-02-17 10:21:13 +0800

    dev-server 访问静态文件目录

--- dev-server.js
+++ dev-server.js
@@ -1,13 +1,14 @@
-var express = require('express')
+var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.use(express.static('build'));
+app.use(express.static(__dirname));
 
-app.listen(8880, function(err) {
+app.listen(8080, function(err) {
 	if(err) {
 		console.log(err);
 		return;
 	}
-	console.log('listening at http://localhost:8880');
+	console.log('listening at http://localhost:8080');
 });
